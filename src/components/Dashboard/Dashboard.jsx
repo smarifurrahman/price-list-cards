19f81ab6dd95f990bb8ae1c4aa35e0606a4e049c
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-04 03:09:32 +0600

    chart style updated

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -19,21 +19,25 @@ const Dashboard = () => {
                 <Line
                     dataKey="physics"
                     stroke="#8884d8"
+                    type="monotone"
                 ></Line>
                 <Line
                     dataKey="mathematics"
                     stroke="#82ca9d"
+                    type="monotone"
                 ></Line>
 
                 <Line
                     dataKey="chemistry"
                     stroke="#82ca1d"
+                    type="monotone"
                 ></Line>
 
                 <XAxis dataKey="name"></XAxis>
                 <YAxis></YAxis>
                 <Tooltip></Tooltip>
                 <Legend></Legend>
+                <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
             </LineChart>
 
         </div>
