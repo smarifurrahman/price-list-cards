18103cf50907ad6cd4dd03cd8a3c1a73692de94e
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-04 17:16:07 +0600

    added axios and a bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar';
 import PriceList from './components/PriceList/PriceList';
 import Dashboard from './components/Dashboard/Dashboard';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
       <h1 className='text-xl'>Tailwind With React</h1>
       <PriceList></PriceList>
       <Dashboard></Dashboard>
+      <PhoneBar></PhoneBar>
     </div>
   )
 }
---
+++ src/components/PhoneBar/PhoneBar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+const PhoneBar = () => {
+    const [phones, setPhones] = useState([]);
+    useEffect(() => {
+        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+        //     .then(res => res.json())
+        //     .then(data => setPhones(data))
+
+        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+            .then(data => {
+                const loadedData = data.data.data;
+                const phonesData = loadedData.map(phone => {
+                    const parts = phone.slug.split('-');
+                    const price = parseInt(parts[1]);
+                    const phoneInfo = {
+                        name: phone.phone_name,
+                        price: price,
+                    }
+                    return phoneInfo;
+                })
+                setPhones(phonesData);
+            })
+    }, [])
+    console.log(phones);
+
+    return (
+        <div>
+            <BarChart
+                width={800}
+                height={300}
+                data={phones}
+            >
+                <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+                <XAxis dataKey="name"></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+                <Legend></Legend>
+                <Bar dataKey="price" fill="#8884d8"></Bar>
+            </BarChart>
+        </div>
+    );
+};
+
+export default PhoneBar;
